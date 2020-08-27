0de3155e4aaef919c1ccd2a81d5ec382a2e8a130
Dean Foster <Deanfoster45@gmail.com>
2020-08-27 17:02:59 -0400

    Updates

---
+++ precache-manifest.ea43cc961d8b999149464e5753f3c5d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed21c2d74bd6e358756ce595267f922e",
+    "url": "/react-trivia/index.html"
+  },
+  {
+    "revision": "c4e0c79043e1943cec8c",
+    "url": "/react-trivia/static/css/main.17d88bc6.chunk.css"
+  },
+  {
+    "revision": "4e908013174a85ac8470",
+    "url": "/react-trivia/static/js/2.c0fb23b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-trivia/static/js/2.c0fb23b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e0c79043e1943cec8c",
+    "url": "/react-trivia/static/js/main.1d5e34fd.chunk.js"
+  },
+  {
+    "revision": "6a1fe8f11376bf2a0033",
+    "url": "/react-trivia/static/js/runtime-main.46099e84.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.085fad88be131d9c0310fb65a90cc7a3.js"
+  "/react-trivia/precache-manifest.ea43cc961d8b999149464e5753f3c5d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-trivia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

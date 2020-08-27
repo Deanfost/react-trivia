291ab5f8fbbe28256fca8734579caa7fb6325c8b
Dean Foster <Deanfoster45@gmail.com>
2020-08-27 16:53:40 -0400

    Updates

---
+++ precache-manifest.085fad88be131d9c0310fb65a90cc7a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f231ef466cc3ebb2af1a485165e8a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de1589d7482208286abb",
+    "url": "/static/css/main.17d88bc6.chunk.css"
+  },
+  {
+    "revision": "4e908013174a85ac8470",
+    "url": "/static/js/2.c0fb23b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c0fb23b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1589d7482208286abb",
+    "url": "/static/js/main.5010ea66.chunk.js"
+  },
+  {
+    "revision": "65f87226063b599d687d",
+    "url": "/static/js/runtime-main.93158592.js"
+  }
+]);

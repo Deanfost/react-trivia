f5c8f63c4f44e016fa004e02ee7323c2bb36cbc5
Dean Foster <Deanfoster45@gmail.com>
2020-08-31 20:58:38 -0400

    Add attribution for OpenTDB

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,17 @@ import './Footer.css';
 
 const Footer = _ => (
     <footer className="Footer">
+        <a href="https://ionicons.com">
+            <ion-icon name="logo-ionic"></ion-icon>
+            <p>Icons by Ionic</p>
+        </a>
         <a href="https://github.com/Deanfost/react-trivia">
             <ion-icon name="logo-github"></ion-icon>
             <p>View on Github</p>
         </a>
-        <a href="https://ionicons.com">
-            <ion-icon name="logo-ionic"></ion-icon>
-            <p>Icons by Ionic</p>
+        <a href="https://opentdb.com">
+            <ion-icon name="server"></ion-icon>
+            <p>Trivia by OpenTDB</p>
         </a>
     </footer>
 );
